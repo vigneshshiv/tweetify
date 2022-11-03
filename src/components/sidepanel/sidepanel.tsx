e98d9ecd00bf0d06afdc98638b48094aab0558c8
vigneshshiv <vichuviky@gmail.com>
2022-11-03 19:16:00 +0530

    feat: sidepanel transparent background removed for small screens

--- src/components/sidepanel/sidepanel.tsx
+++ src/components/sidepanel/sidepanel.tsx
@@ -10,11 +10,11 @@ import TemplateCard from './templatecard';
 const SidePanel = ({ isPanelOpen }: { isPanelOpen: boolean }): JSX.Element => {
   return (
     <aside
-      className={cn('h-full w-60 self-end overflow-hidden p-2 transition-transform duration-200 ease-out lg:translate-x-0', 
+      className={cn('h-full w-60 self-end overflow-hidden p-2 transition-transform duration-200 ease-out lg:translate-x-0 ', 
         isPanelOpen ? 'translate-x-0' : 'translate-x-60')}
     >
       <div
-        className='scrollbar h-full overflow-y-auto rounded-md text-sm shadow-[1px_1px_5px_rgba(0,0,0,0.25)] dark:bg-secondary-dark dark:[&_h2]:text-white'
+        className='scrollbar h-full overflow-y-auto rounded-md bg-white text-sm shadow-[1px_1px_5px_rgba(0,0,0,0.25)] dark:bg-secondary-dark dark:[&_h2]:text-white'
       >
         <SizeCard />
         <ColorCard />
